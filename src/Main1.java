fa87a5e1b0d5de03a0456d5247041b8e19978e55
Jingbug <rjy747@gmail.com>
2025-04-02 01:15:32 +0900

    fix : sout

--- src/Main1.java
+++ src/Main1.java
@@ -3,6 +3,5 @@ public class Main1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My name is Java");
-        System.out.println("Hello World");
     }
 }
